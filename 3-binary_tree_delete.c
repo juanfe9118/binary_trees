abdcd1ff0e2ef287116a92fa8051907fae06044f
juanfe9118 <1168@holbertonschool.com>
2020-03-10 18:17:30 -0500

    Add 5th task and first part of 4th task

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,12 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer od the root node of the tree to delete
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+}
