36623a3a1928a2642f20ca574f5cdd38a7607c6b
juanfe9118 <1168@holbertonschool.com>
2020-03-11 21:57:07 -0500

    Modify condition

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,10 +8,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
-		return;
-
-	func(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	while (root)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
