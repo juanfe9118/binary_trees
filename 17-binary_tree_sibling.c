55ad00ef0fe582049c8f835e7a23ac3347a5a3b5
juanfe9118 <1168@holbertonschool.com>
2020-03-12 23:48:06 -0500

    Fix conditions

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,16 +10,13 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left->n == node->n)
-	{
-		if (node->parent->right != NULL)
-			return (node->parent->right);
-	}
-	else if (node->parent->right->n == node->n)
-	{
-		if (node->parent->left != NULL)
-			return (node->parent->left);
-	}
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 
 	return (NULL);
 }
